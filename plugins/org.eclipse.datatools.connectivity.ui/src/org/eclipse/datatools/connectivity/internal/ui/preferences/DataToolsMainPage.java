2009ace9acdb552899537e1ddcb7e321e99afa88
jgraham <>
2006-01-23 22:50:19 +0000

    [124952] Remove Eclipse 3.2 specific values from manifest file. Add "Data Tools" preference category to connectivity.ui

---
+++ plugins/org.eclipse.datatools.connectivity.ui/src/org/eclipse/datatools/connectivity/internal/ui/preferences/DataToolsMainPage.java
@@ -0,0 +1,32 @@
+package org.eclipse.datatools.connectivity.internal.ui.preferences;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class DataToolsMainPage extends PreferencePage implements
+		IWorkbenchPreferencePage {
+
+	public DataToolsMainPage() {
+		super();
+	}
+
+	public DataToolsMainPage(String title) {
+		super(title);
+	}
+
+	public DataToolsMainPage(String title, ImageDescriptor image) {
+		super(title, image);
+	}
+
+	protected Control createContents(Composite parent) {
+		return null;
+	}
+
+	public void init(IWorkbench workbench) {
+	}
+
+}
