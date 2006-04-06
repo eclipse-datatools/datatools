7b1663e85fd6198c0576f737813bc79f5e66ef54
hcao <>
2006-04-06 01:31:04 +0000

    *** empty log message ***

--- plugins/org.eclipse.datatools.sqltools.plan/src/org/eclipse/datatools/sqltools/plan/internal/ui/view/PlansDialog.java
+++ plugins/org.eclipse.datatools.sqltools.plan/src/org/eclipse/datatools/sqltools/plan/internal/ui/view/PlansDialog.java
@@ -1,8 +1,14 @@
-/**
- * Created on 2004-11-22
- * 
- * Copyright (c) Sybase, Inc. 2004-2006 All rights reserved.
- */
+/*******************************************************************************
+ * Copyright (c) 2004, 2005 Sybase, Inc. and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Sybase, Inc. - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.datatools.sqltools.plan.internal.ui.view;
 
 import java.util.ArrayList;
