8481ef91bdaf1d11ba98f086c27c06d0de909769
hcao <>
2006-04-06 01:19:25 +0000

    *** empty log message ***

--- plugins/org.eclipse.datatools.sqltools.sqleditor/src/org/eclipse/datatools/sqltools/sqleditor/result/SimpleSQLResultRunnable.java
+++ plugins/org.eclipse.datatools.sqltools.sqleditor/src/org/eclipse/datatools/sqltools/sqleditor/result/SimpleSQLResultRunnable.java
@@ -1,6 +1,14 @@
-/*
- * Confidential Property of Sybase, Inc. (c) Copyright Sybase, Inc. 2004-2006. All rights reserved.
- */
+/*******************************************************************************
+ * Copyright (c) 2004, 2005 Sybase, Inc. and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Sybase, Inc. - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.datatools.sqltools.sqleditor.result;
 
 import java.sql.Connection;
