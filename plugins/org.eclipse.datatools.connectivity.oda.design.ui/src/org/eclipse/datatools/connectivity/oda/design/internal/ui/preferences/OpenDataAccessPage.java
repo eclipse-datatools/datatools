9414774eff9af07ae0b8a6c4c5a084d027e4e519
Linda Chan <lchan@actuate.com>
2006-10-16 09:31:27 +0000

    [161019] Open Data Access Preference Page

---
+++ plugins/org.eclipse.datatools.connectivity.oda.design.ui/src/org/eclipse/datatools/connectivity/oda/design/internal/ui/preferences/OpenDataAccessPage.java
@@ -0,0 +1,64 @@
+/*
+ *************************************************************************
+ * Copyright (c) 2006 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation - initial API and implementation
+ *  
+ *************************************************************************
+ */
+
+package org.eclipse.datatools.connectivity.oda.design.internal.ui.preferences;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+/**
+ * Preference page for the Open Data Access framework.
+ */
+public class OpenDataAccessPage extends PreferencePage implements
+        IWorkbenchPreferencePage
+{
+
+    public OpenDataAccessPage()
+    {
+        super();
+        noDefaultAndApplyButton();
+    }
+
+    public OpenDataAccessPage( String title )
+    {
+        super( title );
+    }
+
+    public OpenDataAccessPage( String title, ImageDescriptor image )
+    {
+        super( title, image );
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.jface.preference.PreferencePage#createContents(org.eclipse.swt.widgets.Composite)
+     */
+    protected Control createContents( Composite parent )
+    {
+        // TODO - framework preferences
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
+     */
+    public void init( IWorkbench workbench )
+    {
+        // do nothing
+    }
+
+}
