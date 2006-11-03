0095ae1d8f295e6195bc872bf265660f562d45a3
rcernich <>
2006-11-03 20:48:12 +0000

    [163333] remove dependency on JDBCDatabase.

--- plugins/org.eclipse.datatools.enablement.mysql/src/org/eclipse/datatools/enablement/mysql/catalog/MySqlCatalogDatabase.java
+++ plugins/org.eclipse.datatools.enablement.mysql/src/org/eclipse/datatools/enablement/mysql/catalog/MySqlCatalogDatabase.java
@@ -1,20 +1,21 @@
- /*******************************************************************************
-  * Copyright (c) 2005 Versant Corporation and others.
-  * All rights reserved. This program and the accompanying materials
-  * are made available under the terms of the Eclipse Public License v1.0
-  * which accompanies this distribution, and is available at
-  * http://www.eclipse.org/legal/epl-v10.html
-  * 
-  * Contributors:
-  *     Versant Corporation - initial API and implementation
-  *******************************************************************************/
- package org.eclipse.datatools.enablement.mysql.catalog;
+/*******************************************************************************
+ * Copyright (c) 2005 Versant Corporation and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Versant Corporation - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.datatools.enablement.mysql.catalog;
 
 import java.sql.Connection;
 
-import org.eclipse.datatools.connectivity.sqm.internal.core.rte.jdbc.JDBCDatabase;
+import org.eclipse.datatools.connectivity.sqm.internal.core.rte.ICatalogObject;
+import org.eclipse.datatools.connectivity.sqm.internal.core.rte.RefreshManager;
+import org.eclipse.datatools.modelbase.sql.schema.Database;
 import org.eclipse.datatools.modelbase.sql.schema.SQLSchemaPackage;
 import org.eclipse.datatools.modelbase.sql.schema.Schema;
+import org.eclipse.datatools.modelbase.sql.schema.impl.DatabaseImpl;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.util.EObjectWithInverseResolvingEList;
 
@@ -22,14 +23,37 @@ import org.eclipse.emf.ecore.util.EObjectWithInverseResolvingEList;
  * This is the Database implementation, and contains schemas (MySql does not
  * have schemas)
  */
-public class MySqlCatalogDatabase extends JDBCDatabase {
+public class MySqlCatalogDatabase extends DatabaseImpl implements ICatalogObject {
 
 	private static final long serialVersionUID = 3906091161042301493L;
 
+	private Connection connection;
+
 	public MySqlCatalogDatabase(Connection connection) {
-		super(connection);
+		super();
+		if (connection == null) {
+			System.err.println("null connection"); //$NON-NLS-1$
+			throw new RuntimeException();
+		}
+		this.connection = connection;
 	}
 
+	public void refresh() {
+		if (schemas != null) {
+			schemas.clear();
+			schemas = null;
+		}
+		RefreshManager.getInstance().referesh(this);
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public Database getCatalogDatabase() {
+		return this;
+	}
+	
 	public EList getSchemas() {
 		if (schemas == null) {
 			schemas = new EObjectWithInverseResolvingEList(Schema.class, this,
