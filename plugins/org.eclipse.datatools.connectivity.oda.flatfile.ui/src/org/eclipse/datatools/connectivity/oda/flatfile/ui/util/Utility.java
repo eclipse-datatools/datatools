f5563e7b842a45c3ab508cbd46504211a07cd95b
Lin Zhu <lzhu@actuate.com>
2006-12-07 10:05:56 +0000

    Fix Bugzilla Bug 147259 [Context Sensitive] The context help button doesn't work when new a flatfile data source/set

---
+++ plugins/org.eclipse.datatools.connectivity.oda.flatfile.ui/src/org/eclipse/datatools/connectivity/oda/flatfile/ui/util/Utility.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2005 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.datatools.connectivity.oda.flatfile.ui.util;
+
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * 
+ */
+
+public class Utility
+{
+
+	/**
+	 * Set context-sensitive help
+	 * 
+	 * @param control
+	 * @param contextId
+	 */
+	public static void setSystemHelp( Control control, String contextId )
+	{
+		PlatformUI.getWorkbench( )
+				.getHelpSystem( )
+				.setHelp( control, contextId );
+	}
+
+}
