bf823cf292ac46471e69d8386582ce2bdd28cb38
rcernich <>
2007-04-05 00:50:52 +0000

    [121957] Allow user to connect from connection profile version property page so that the version information can be updated directly from the property page.

--- plugins/org.eclipse.datatools.connectivity.ui/src/org/eclipse/datatools/connectivity/internal/ui/wizards/CPVersionPropertyPage.java
+++ plugins/org.eclipse.datatools.connectivity.ui/src/org/eclipse/datatools/connectivity/internal/ui/wizards/CPVersionPropertyPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005 Sybase, Inc.
+ * Copyright (c) 2005-2007 Sybase, Inc.
  * 
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
@@ -11,6 +11,7 @@
 package org.eclipse.datatools.connectivity.internal.ui.wizards;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -19,10 +20,16 @@ import java.util.Properties;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.datatools.connectivity.ConnectionProfileConstants;
 import org.eclipse.datatools.connectivity.IConnectionProfile;
+import org.eclipse.datatools.connectivity.IPropertySetChangeEvent;
+import org.eclipse.datatools.connectivity.IPropertySetListener;
+import org.eclipse.datatools.connectivity.IPropertySetChangeEvent.IChangedProperty;
 import org.eclipse.datatools.connectivity.internal.ui.ConnectivityUIPlugin;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
@@ -38,6 +45,43 @@ public class CPVersionPropertyPage extends PropertyPage implements
 		IWorkbenchPropertyPage {
 
 	private static final int INDENT_TECH_VERSION = 20;
+
+	private Button mUpdateVersionInfoButton;
+
+	private IPropertySetListener mPropertySetListener = new IPropertySetListener() {
+
+		public void propertySetChanged(IPropertySetChangeEvent event) {
+			if (ConnectionProfileConstants.VERSION_INFO_PROFILE_EXTENSION_ID
+					.equals(event.getPropertySetType())) {
+				getControl().getDisplay().asyncExec(new Runnable() {
+
+					public void run() {
+						updateVersionInformation();
+					}
+				});
+			}
+			else if (IConnectionProfile.CONNECTION_PROFILE_PROPERTY_SET
+					.equals(event.getPropertySetType())) {
+				final IChangedProperty icp = event
+						.getChangedProperty(IConnectionProfile.CONNECTED_PROPERTY_ID);
+				if (icp != null) {
+					getControl().getDisplay().asyncExec(new Runnable() {
+
+						public void run() {
+							if (Boolean.valueOf(icp.getNewValue())
+									.booleanValue()) {
+								updateVersionInformation();
+								mUpdateVersionInfoButton.setEnabled(false);
+							}
+							else {
+								mUpdateVersionInfoButton.setEnabled(true);
+							}
+						}
+					});
+				}
+			}
+		}
+	};
 
 	/**
 	 * 
@@ -57,12 +101,37 @@ public class CPVersionPropertyPage extends PropertyPage implements
 		GridLayout layout = new GridLayout();
 		content.setLayout(layout);
 
-		IConnectionProfile profile = getConnectionProfile();
-		setDescription(ConnectivityUIPlugin.getDefault()
-				.getResourceString("CPVersionPropertyPage.desc", //$NON-NLS-1$
-						new Object[] { profile.getName()}));
+		createControls(content);
 
-		Properties props = profile.getProperties(ConnectionProfileConstants.VERSION_INFO_PROFILE_EXTENSION_ID);
+		getConnectionProfile().addPropertySetListener(mPropertySetListener);
+
+		return content;
+	}
+
+	public void dispose() {
+		getConnectionProfile().removePropertySetListener(mPropertySetListener);
+		super.dispose();
+	}
+
+	private void updateVersionInformation() {
+		Composite content = (Composite) getControl();
+		if (content == null || content.isDisposed()) {
+			return;
+		}
+		deleteControls(content);
+		createControls(content);
+		content.layout(true);
+		content.redraw();
+	}
+
+	private void createControls(Composite content) {
+		IConnectionProfile profile = getConnectionProfile();
+		setDescription(ConnectivityUIPlugin.getDefault().getResourceString(
+				"CPVersionPropertyPage.desc", //$NON-NLS-1$
+				new Object[] { profile.getName()}));
+
+		Properties props = profile
+				.getProperties(ConnectionProfileConstants.VERSION_INFO_PROFILE_EXTENSION_ID);
 		if (props.getProperty(ConnectionProfileConstants.PROP_SERVER_NAME) == null) {
 			Label label = new Label(content, SWT.NULL);
 			label.setText(ConnectivityUIPlugin.getDefault().getResourceString(
@@ -72,7 +141,7 @@ public class CPVersionPropertyPage extends PropertyPage implements
 		}
 		else {
 			Composite serverVersionContainer = new Composite(content, SWT.NULL);
-			layout = new GridLayout(2, false);
+			GridLayout layout = new GridLayout(2, false);
 			layout.marginHeight = layout.marginWidth = 0;
 			serverVersionContainer.setLayout(layout);
 			GridData data = new GridData(GridData.FILL_HORIZONTAL);
@@ -142,9 +211,31 @@ public class CPVersionPropertyPage extends PropertyPage implements
 			}
 		}
 
-		return content;
+		mUpdateVersionInfoButton = new Button(content, SWT.PUSH);
+		mUpdateVersionInfoButton
+				.setText(ConnectivityUIPlugin.getDefault().getResourceString(
+						"CPVersionPropertyPage.button.updateVersion"));
+		mUpdateVersionInfoButton.setLayoutData(new GridData(
+				GridData.HORIZONTAL_ALIGN_BEGINNING
+						| GridData.VERTICAL_ALIGN_END));
+		mUpdateVersionInfoButton.addSelectionListener(new SelectionAdapter() {
+
+			public void widgetSelected(SelectionEvent event) {
+				getConnectionProfile().connect(null);
+			}
+		});
+
+		mUpdateVersionInfoButton.setEnabled(!getConnectionProfile()
+				.isConnected());
 	}
-	
+
+	private void deleteControls(Composite content) {
+		for (Iterator it = Arrays.asList(content.getChildren()).iterator(); it
+				.hasNext();) {
+			((Control) it.next()).dispose();
+		}
+	}
+
 	private IConnectionProfile getConnectionProfile() {
 		IAdaptable element = getElement();
 		IConnectionProfile profile = (IConnectionProfile) element
