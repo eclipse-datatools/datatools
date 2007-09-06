de06c74ccf447a71b6f8ecb052c14cbfee94d175
hcao <>
2007-09-06 12:08:01 +0000

    *** empty log message ***

--- plugins/org.eclipse.datatools.sqltools.sqleditor/src/org/eclipse/datatools/sqltools/sqleditor/IHelpConstants.java
+++ plugins/org.eclipse.datatools.sqltools.sqleditor/src/org/eclipse/datatools/sqltools/sqleditor/IHelpConstants.java
@@ -1,8 +1,14 @@
-/**
- * Created on 2007-9-6
- * 
- * Copyright (c) Sybase, Inc. 2004-2007. All rights reserved.
- */
+/*******************************************************************************
+ * Copyright (c) 2004, 2005 Sybase, Inc. and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Sybase, Inc. - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.datatools.sqltools.sqleditor;
 
 import org.eclipse.datatools.sqltools.sqleditor.internal.SQLEditorPlugin;
