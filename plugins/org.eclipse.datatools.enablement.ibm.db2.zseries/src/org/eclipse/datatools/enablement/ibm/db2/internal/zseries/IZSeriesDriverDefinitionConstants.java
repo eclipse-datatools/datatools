afe8e43401cc5d236d2f482b300f961adeef9c6c
ledunnel <>
2007-10-18 20:36:17 +0000

    [206816]  DB2 for z/OS:  Need a way for user to specify filter based on TB creator

---
+++ plugins/org.eclipse.datatools.enablement.ibm.db2.zseries/src/org/eclipse/datatools/enablement/ibm/db2/internal/zseries/IZSeriesDriverDefinitionConstants.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.datatools.enablement.ibm.db2.internal.zseries;
+
+public interface IZSeriesDriverDefinitionConstants {
+	public static final String FILTER_ON_TBCREATOR_PROPERTY_ID ="org.eclipse.datatools.enablement.ibm.db2.zseries.filterOnTBCreator"; //$NON-NLS-1$
+	public static final String FILTER_ON_TBCREATOR_VALUE_TRUE ="true"; //$NON-NLS-1$
+	public static final String FILTER_ON_TBCREATOR_VALUE_FALSE ="false"; //$NON-NLS-1$
+}
