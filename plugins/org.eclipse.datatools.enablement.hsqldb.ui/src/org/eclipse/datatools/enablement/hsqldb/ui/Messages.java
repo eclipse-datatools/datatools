2172d83cd6b98ab63b49df81c86b88693c3975e3
ledunnel <>
2008-01-18 19:47:23 +0000

    [203158]  Need strict separation between UI and non-UI enablement plug-ins

---
+++ plugins/org.eclipse.datatools.enablement.hsqldb.ui/src/org/eclipse/datatools/enablement/hsqldb/ui/Messages.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2007, 2008 Sybase, Inc.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Brian Fitzpatrick - initial API and implementation
+ * 			IBM Corporation - refactored plug-in
+ ******************************************************************************/
+package org.eclipse.datatools.enablement.hsqldb.ui;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+/**
+ * @author brianf
+ *
+ */
+public class Messages {
+	
+	private static final String BUNDLE_NAME = "org.eclipse.datatools.enablement.hsqldb.ui.messages"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+			.getBundle(BUNDLE_NAME);
+
+	/**
+	 * 
+	 */
+	private Messages() {
+	}
+
+	/**
+	 * @param key
+	 * @return
+	 */
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
