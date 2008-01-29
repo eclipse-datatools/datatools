3f5fc950b15a4f9529c90f5e919ca0ec0b679878
Wolfgang Auer <wolfgang.auer@sap.com>
2008-01-29 13:47:00 +0000

    Initial MaxDB contribution for DTP. BZ https://bugs.eclipse.org/bugs/show_bug.cgi?id=197806

---
+++ plugins/org.eclipse.datatools.enablement.sap.maxdb.ui/src/org/eclipse/datatools/enablement/sap/maxdb/ui/MaxDBUIPlugin.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2008 SAP AG
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Dimiter Dimitrov; Wolfgang Auer - initial API and implementation
+ *******************************************************************************/ 
+
+package org.eclipse.datatools.enablement.sap.maxdb.ui;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+
+public class MaxDBUIPlugin extends AbstractUIPlugin {
+	
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.datatools.enablement.sap.maxdb.ui"; //$NON-NLS-1$	
+	
+	// The shared instance
+	private static MaxDBUIPlugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public MaxDBUIPlugin() {
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static MaxDBUIPlugin getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+	
+
+}
---
+++ plugins/org.eclipse.datatools.enablement.sap.maxdb/src/org/eclipse/datatools/enablement/sap/maxdb/MaxDBTableConstraintLoader.java
@@ -0,0 +1,179 @@
+/*******************************************************************************
+ * Copyright (c) 2008 SAP AG
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Dimiter Dimitrov; Wolfgang Auer - initial API and implementation
+ *******************************************************************************/ 
+
+package org.eclipse.datatools.enablement.sap.maxdb;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.eclipse.datatools.connectivity.sqm.core.rte.ICatalogObject;
+import org.eclipse.datatools.connectivity.sqm.loader.IConnectionFilterProvider;
+import org.eclipse.datatools.connectivity.sqm.loader.JDBCTableConstraintLoader;
+import org.eclipse.datatools.modelbase.sql.constraints.PrimaryKey;
+import org.eclipse.datatools.modelbase.sql.constraints.UniqueConstraint;
+import org.eclipse.datatools.modelbase.sql.tables.Table;
+
+//this class is a workaround for the DTP bug 193957 and 197806
+public class MaxDBTableConstraintLoader extends JDBCTableConstraintLoader{
+
+	public MaxDBTableConstraintLoader() {
+		super(null);
+	}
+
+
+	/**
+	 * This constructs the loader using no filter.
+	 *
+	 * @param catalogObject the Table object upon which this loader operates.
+	 */
+	public MaxDBTableConstraintLoader(ICatalogObject catalogObject) {
+		this(catalogObject, null);
+	}
+
+	/**
+	 * @param catalogObject the Catalog object upon which this loader operates.
+	 * @param connectionFilterProvider the filter provider used for filtering
+	 *        the "constraint" objects being loaded
+	 */
+	public MaxDBTableConstraintLoader(
+										ICatalogObject catalogObject,
+										IConnectionFilterProvider connectionFilterProvider) {
+		super(catalogObject, connectionFilterProvider);
+		assert (catalogObject instanceof Table);
+	}
+
+	/**
+	 * Loads the "primary key" object from the database. This method uses the
+	 * result set from createPrimaryKeyResultSet() to load the "primary key"
+	 * object from the server..
+	 *
+	 * @param existingPK the existing primary key, if one exists.
+	 * @return the table's primary key
+	 *
+	 * @throws SQLException if an error occurred during loading.
+	 */
+	// FIXME: this method is a workaround for the DTP bug 189079 -> to be removed	
+	public PrimaryKey loadPrimaryKey(PrimaryKey existingPK) throws SQLException {
+		ResultSet rs = null;
+		try {
+			Map columns = new TreeMap();
+			PrimaryKey pk = null;
+			for (rs = createPrimaryKeyResultSet(); rs.next();) {
+				if (pk == null) {
+					String pkName = rs.getString(COLUMN_PK_NAME);
+					if (pkName == null) {
+						pkName = rs.getString("TABLE_NAME")+"_PK";
+					}
+					if (existingPK != null
+							&& pkName.equals(existingPK.getName())) {
+						pk = existingPK;
+						pk.getMembers().clear();
+						if (existingPK instanceof ICatalogObject) {
+							((ICatalogObject) pk).refresh();
+						}
+					}
+					else {
+						pk = createPrimaryKey();
+						pk.setName(pkName);
+					}
+				}
+				columns.put(new Integer(rs.getShort(COLUMN_KEY_SEQ)),
+						findColumn(rs.getString(COLUMN_COLUMN_NAME)));
+			}
+			for (Iterator it = columns.values().iterator(); it.hasNext();) {
+				pk.getMembers().add(it.next());
+			}
+			return pk;
+		}
+		finally {
+			if (rs != null) {
+				closeResultSet(rs);
+			}
+		}
+	}
+	
+	/**
+	 * Loads the "unique constraint" objects from the database. This method uses
+	 * the result set from createUniqueConstraintResultSet() to load the "unique
+	 * constraint" objects from the server.
+	 * 
+	 * @param pk the table's primary key. Used to prevent duplicating the PK
+	 *        constraint.
+	 * @param containmentList the containment list held by parent
+	 * @param existingUCs the catalog objects which were previously loaded
+	 * 
+	 * @throws SQLException if an error occurred during loading.
+	 */
+	public void loadUniqueConstraints(PrimaryKey pk, List containmentList,Collection existingUCs) throws SQLException {
+		ResultSet rs = null;
+		if (pk != null) {
+			// Remove this guy from the list.
+			existingUCs.remove(pk);
+		}
+		try {
+			Map constraints = new HashMap();
+			Map constraintColumns = new HashMap();
+			for (rs = createUniqueConstraintResultSet(); rs.next();) {
+				String ucName = rs.getString(COLUMN_PK_NAME);				
+				if (ucName == null) {
+					ucName = rs.getString(COLUMN_PKTABLE_NAME)+"_PK";
+				}
+				
+				if (ucName.equals(pk == null ? null : pk.getName())) {
+					// Already seen this guy
+					continue;
+				}
+				else if (!constraints.containsKey(ucName)) {
+					UniqueConstraint uc = (UniqueConstraint) getAndRemoveSQLObject(
+							existingUCs, ucName);
+					if (uc == null) {
+						// create the next UC
+						uc = createUniqueConstraint();
+						uc.setName(ucName);
+					}
+					else {
+						uc.getMembers().clear();
+						if (uc instanceof ICatalogObject) {
+							((ICatalogObject) uc).refresh();
+						}
+					}
+					containmentList.add(uc);
+					constraints.put(ucName, uc);
+					constraintColumns.put(ucName, new TreeMap());
+				}
+				((Map) constraintColumns.get(ucName)).put(new Integer(rs
+						.getShort(COLUMN_KEY_SEQ)), findColumn(rs
+						.getString(COLUMN_PKCOLUMN_NAME)));
+			}
+			for (Iterator it = constraints.entrySet().iterator(); it.hasNext();) {
+				Map.Entry entry = (Map.Entry) it.next();
+				UniqueConstraint uc = (UniqueConstraint) entry.getValue();
+				for (Iterator colIt = ((Map) constraintColumns
+						.get(uc.getName())).values().iterator(); colIt
+						.hasNext();) {
+					uc.getMembers().add(colIt.next());
+				}
+			}
+		}
+		finally {
+			if (rs != null) {
+				closeResultSet(rs);
+			}
+		}
+	}
+
+}
