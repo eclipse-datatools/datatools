8fc878d43269cd7976830758c5d542ac8018e062
hcao <>
2008-02-28 11:54:56 +0000

    *** empty log message ***

--- tests/org.eclipse.datatools.sqltools.test/src/org/eclipse/datatools/sqltools/sql/util/test/ModelUtilTest.java
+++ tests/org.eclipse.datatools.sqltools.test/src/org/eclipse/datatools/sqltools/sql/util/test/ModelUtilTest.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2005 Sybase, Inc. and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Sybase, Inc. - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.datatools.sqltools.sql.util.test;
 
 import junit.framework.TestCase;
@@ -9,6 +20,11 @@ import org.eclipse.datatools.modelbase.sql.schema.impl.SQLSchemaFactoryImpl;
 import org.eclipse.datatools.sqltools.sql.util.ModelUtil;
 import org.eclipse.emf.common.util.EList;
 
+/**
+ * 
+ * @author Hui Cao
+ *
+ */
 public class ModelUtilTest extends TestCase {
 
 	public void testGetSchemas() {
