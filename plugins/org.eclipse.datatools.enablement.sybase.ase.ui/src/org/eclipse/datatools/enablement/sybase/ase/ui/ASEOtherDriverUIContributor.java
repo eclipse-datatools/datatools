b5739087a19c4131fdf435a61e133498c4547ab7
Brian Fitzpatrick <bfitzpat@redhat.com>
2008-04-08 21:11:26 +0000

    NEW - bug 225025: Sybase 15.x  and 12.x Other Driver has no UI https://bugs.eclipse.org/bugs/show_bug.cgi?id=225025 NEW - bug 225014: Bundle version ranges missing for many Sybase enablement plug-ins https://bugs.eclipse.org/bugs/show_bug.cgi?id=225014 as well as bug 225024 and bug 225019

---
+++ plugins/org.eclipse.datatools.enablement.sybase.ase.ui/src/org/eclipse/datatools/enablement/sybase/ase/ui/ASEOtherDriverUIContributor.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.datatools.enablement.sybase.ase.ui;
+
+import org.eclipse.datatools.connectivity.ui.wizards.OtherDriverUIContributor;
+
+public class ASEOtherDriverUIContributor extends OtherDriverUIContributor {
+
+}
