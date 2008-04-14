abeb3c36e0a97c9403a35a899e4e733b4762344d
Brian Fitzpatrick <bfitzpat@redhat.com>
2008-04-14 14:56:22 +0000

    NEW - bug 226848: Sybase confidential notices appear instead of EPL https://bugs.eclipse.org/bugs/show_bug.cgi?id=226848

--- plugins/org.eclipse.datatools.enablement.sybase.ase/src/org/eclipse/datatools/enablement/ase/JDBCASEProfileMessages.java
+++ plugins/org.eclipse.datatools.enablement.sybase.ase/src/org/eclipse/datatools/enablement/ase/JDBCASEProfileMessages.java
@@ -1,8 +1,13 @@
-//
-//Confidential property of Sybase, Inc.
-//(c) Copyright Sybase, Inc. 2004.
-//All rights reserved
-//
+/*******************************************************************************
+ * Copyright (c) 2008 Sybase, Inc.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Sybase - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.datatools.enablement.ase;
 
 import java.text.MessageFormat;
