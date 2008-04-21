17b00e26b2fadb00e0d2195f3dc8fa4e499fd200
Brian Fitzpatrick <bfitzpat@redhat.com>
2008-04-21 15:21:24 +0000

    NEW - bug 228026: Missing EPL from several file containing Sybase copyright. https://bugs.eclipse.org/bugs/show_bug.cgi?id=228026

--- plugins/org.eclipse.datatools.enablement.sybase.ase/src/org/eclipse/datatools/enablement/ase/containment/SybaseASERuleContainmentProvider.java
+++ plugins/org.eclipse.datatools.enablement.sybase.ase/src/org/eclipse/datatools/enablement/ase/containment/SybaseASERuleContainmentProvider.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Sybase, Inc.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Sybase
+ ******************************************************************************/
 package org.eclipse.datatools.enablement.ase.containment;
 
 import org.eclipse.datatools.connectivity.sqm.core.containment.AbstractContainmentProvider;
