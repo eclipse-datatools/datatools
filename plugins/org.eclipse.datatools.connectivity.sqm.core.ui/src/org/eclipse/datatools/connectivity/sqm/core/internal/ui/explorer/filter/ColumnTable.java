6cd161c1243e429341bfeaa9c55e579ae725c11f
ledunnel <>
2008-06-06 19:25:44 +0000

    [236110]  Various copyright issues in connectivity plug-ins

--- plugins/org.eclipse.datatools.connectivity.sqm.core.ui/src/org/eclipse/datatools/connectivity/sqm/core/internal/ui/explorer/filter/ColumnTable.java
+++ plugins/org.eclipse.datatools.connectivity.sqm.core.ui/src/org/eclipse/datatools/connectivity/sqm/core/internal/ui/explorer/filter/ColumnTable.java
@@ -1,12 +1,12 @@
-/*
- *+------------------------------------------------------------------------+
- *| Licensed Materials - Property of IBM                                   |
- *| (C) Copyright IBM Corp. 2003, 2004.  All Rights Reserved.              |
- *|                                                                        |
- *| US Government Users Restricted Rights - Use, duplication or disclosure |
- *| restricted by GSA ADP Schedule Contract with IBM Corp.                 |
- *+------------------------------------------------------------------------+
- */
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
 package org.eclipse.datatools.connectivity.sqm.core.internal.ui.explorer.filter;
 
 import java.util.ArrayList;
