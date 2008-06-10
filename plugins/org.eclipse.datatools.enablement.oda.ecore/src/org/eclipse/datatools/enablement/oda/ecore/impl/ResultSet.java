d723686229392ea1d2debe043b7fb16c15e51e31
tmyer <>
2008-06-10 22:51:39 +0000

    Re-added suppress warnings flag.

--- plugins/org.eclipse.datatools.enablement.oda.ecore/src/org/eclipse/datatools/enablement/oda/ecore/impl/ResultSet.java
+++ plugins/org.eclipse.datatools.enablement.oda.ecore/src/org/eclipse/datatools/enablement/oda/ecore/impl/ResultSet.java
@@ -48,6 +48,7 @@ public class ResultSet implements IResultSet {
 	private final Iterator<EObject> queryResultIterator;
 	private final Query query;
 
+	@SuppressWarnings("unchecked")
 	public ResultSet(final Query query, final ColumnDefinition[] columns, final int maxRows) {
 		this.query = query;
 		this.columns = columns;
