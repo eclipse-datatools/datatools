26c257871da1323770b8a012ddc3ae626c2ac0cc
Brian Payton <bpayton@us.ibm.com>
2009-05-20 22:45:46 +0000

    [275856] Modified DataFileTokenizer in plugin o.e.d.sqltools.data.core to check for BOM encoding when reading file

--- plugins/org.eclipse.datatools.sqltools.data.core/src/org/eclipse/datatools/sqltools/data/internal/core/load/DataFileTokenizer.java
+++ plugins/org.eclipse.datatools.sqltools.data.core/src/org/eclipse/datatools/sqltools/data/internal/core/load/DataFileTokenizer.java
@@ -36,6 +36,29 @@ public class DataFileTokenizer
     }
     
     /**
+     * The BOM (Byte Order Marks) is a Unicode special marker placed at the top of the file that indicate its encoding. 
+     * The UTF-8 representation of the BOM is the byte sequence EF BB BF. The BOM is optional for UTF-8.
+     * Before loading the file, a check is made if BOM are introduced in the UTF-8 encoded file. 
+     * If the first three bytes are 0xEF, 0xBB, 0xBF the first character is skipped as it is BOM.
+     */
+    public boolean bomEncoding() throws IOException
+    {
+    	FileInputStream fis=new FileInputStream(filePath);
+    	boolean isBOM=false;
+    	int byteOne = fis.read();
+		int byteTwo = fis.read();
+		int byteThree = fis.read();
+		
+		//Check the BOM for UTF-8 encoding
+		if (byteOne == 0xEF && byteTwo == 0xBB && byteThree == 0xBF)
+		{
+			// UTF-8
+			isBOM=true;
+		}
+		return isBOM;
+    }
+    
+    /**
      * Opens the extracted file to be loaded into the table.
      * The method assumes the file was saved using UTF-8 character encoding
      */
@@ -44,7 +67,11 @@ public class DataFileTokenizer
     	FileInputStream fis = new FileInputStream(filePath);
 		InputStreamReader isw = new InputStreamReader(fis, "UTF-8");
 		reader = new BufferedReader(isw);    	
-        
+		
+		//Skip the first character if it is BOM
+		if(bomEncoding() == true)
+			reader.skip(1);
+		
         String line = reader.readLine();        
         if (line!=null)
             st = new StringTokenizer(line, delims, true);
