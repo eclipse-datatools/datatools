d05e4df66a33660a9e6c2d05e0182ecf53985ac9
Linda Chan <lchan@actuate.com>
2010-01-11 20:15:50 +0000

    [299176] Provide visibility to a custom ODA Data Set Wizard on the type of its design session

---
+++ plugins/org.eclipse.datatools.enablement.oda.ws.ui/src/org/eclipse/datatools/enablement/oda/ws/ui/wizards/SOAPDataSetWizard.java
@@ -0,0 +1,44 @@
+/*
+ *************************************************************************
+ * Copyright (c) 2010 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation - initial API and implementation
+ *  
+ *************************************************************************
+ */
+
+package org.eclipse.datatools.enablement.oda.ws.ui.wizards;
+
+import org.eclipse.datatools.connectivity.oda.design.ui.wizards.DataSetWizard;
+import org.eclipse.jface.wizard.IWizardPage;
+
+/**
+ *  Extended implementation of the ODA Data Set Wizard.
+ */
+public class SOAPDataSetWizard extends DataSetWizard
+{
+
+    public SOAPDataSetWizard()
+    {
+        super();
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.jface.wizard.Wizard#addPage(org.eclipse.jface.wizard.IWizardPage)
+     */
+    @Override
+    public void addPage( IWizardPage page )
+    {
+        // skip the SOAPParametersPage if editing an existing data set design
+        if( page instanceof SOAPParametersPage && ! isCreatingNewDesign() )
+            return;
+        
+        super.addPage( page );
+    }
+
+}
