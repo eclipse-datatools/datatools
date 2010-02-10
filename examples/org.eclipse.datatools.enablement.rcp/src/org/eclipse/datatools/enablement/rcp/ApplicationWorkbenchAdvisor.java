66ec5156f76efbf54419e62d1583b8f323768736
Brian Fitzpatrick <bfitzpat@redhat.com>
2010-02-10 22:53:53 +0000

    Adding RCP app to Enablement

---
+++ examples/org.eclipse.datatools.enablement.rcp/src/org/eclipse/datatools/enablement/rcp/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Sybase, Inc. and others
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Ingres - initial API and implementation
+ * 				brianf - a few tweaks
+ ******************************************************************************/
+package org.eclipse.datatools.enablement.rcp;
+
+import java.net.URL;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.application.IWorkbenchConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.eclipse.ui.internal.ide.model.WorkbenchAdapterBuilder;
+import org.eclipse.ui.internal.ide.undo.WorkspaceUndoMonitor;
+import org.osgi.framework.Bundle;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "org.eclipse.datatools.enablement.rcp.perspective";
+
+	private transient WorkspaceUndoMonitor workspaceUndoMonitor;
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+	public void initialize(IWorkbenchConfigurer configurer) {
+	    super.initialize(configurer);
+		// make sure we always save and restore workspace state
+		configurer.setSaveAndRestore(true);
+	    WorkbenchAdapterBuilder.registerAdapters();
+
+		final String ICONS_PATH = "icons/full/";
+		final String PATH_OBJECT = ICONS_PATH + "obj16/";
+		Bundle ideBundle = Platform.getBundle(IDEWorkbenchPlugin.IDE_WORKBENCH);
+		declareWorkbenchImage(configurer, ideBundle,
+				IDE.SharedImages.IMG_OBJ_PROJECT, PATH_OBJECT + "prj_obj.gif",
+				true);
+		declareWorkbenchImage(configurer, ideBundle,
+				IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, PATH_OBJECT
+						+ "cprj_obj.gif", true);
+
+		// initialize the workspace undo monitor
+		workspaceUndoMonitor = WorkspaceUndoMonitor.getInstance();
+	}
+
+	private void declareWorkbenchImage(IWorkbenchConfigurer configurer_p,
+			Bundle ideBundle, String symbolicName, String path, boolean shared) {
+		URL url = ideBundle.getEntry(path);
+		ImageDescriptor desc = ImageDescriptor.createFromURL(url);
+		configurer_p.declareImage(symbolicName, desc, shared);
+	}
+
+	@Override
+	public IAdaptable getDefaultPageInput() {
+		return ResourcesPlugin.getWorkspace().getRoot();
+	}
+
+	@Override
+	public void postShutdown() {
+		if (workspaceUndoMonitor != null) {
+			workspaceUndoMonitor.shutdown();
+			workspaceUndoMonitor = null;
+		}
+	}
+}
---
+++ examples/org.eclipse.datatools.enablement.rcp/src/org/eclipse/datatools/enablement/rcp/Perspective.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Sybase, Inc. and others
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Ingres - initial API and implementation
+ * 				brianf - a few tweaks
+ ******************************************************************************/
+package org.eclipse.datatools.enablement.rcp;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+	
+	private static String DSE_VIEW_ID = "org.eclipse.datatools.connectivity.DataSourceExplorerNavigator"; //$NON-NLS-1$
+	private static String PROJECT_EXPLORER_VIEW_ID = "org.eclipse.ui.navigator.ProjectExplorer"; //$NON-NLS-1$
+	private static String RESULT_VIEW_ID = "org.eclipse.datatools.sqltools.result.resultView"; //$NON-NLS-1$
+	private static String PLAN_VIEW_ID = "org.eclipse.datatools.sqltools.plan.planView"; //$NON-NLS-1$
+
+	public void createInitialLayout(IPageLayout layout) {
+
+		// Get the editor area.
+        String editorArea = layout.getEditorArea();
+		layout.setEditorAreaVisible(true);
+
+        //Left
+        IFolderLayout left = layout.createFolder("topleft", IPageLayout.LEFT, 0.25f, editorArea); //$NON-NLS-1$
+        left.addView(DSE_VIEW_ID);
+
+		left = layout.createFolder("bottomleft", IPageLayout.BOTTOM, 0.5f, //$NON-NLS-1$
+			"topleft"); //$NON-NLS-1$
+        left.addView(PROJECT_EXPLORER_VIEW_ID);
+
+        // Bottom 
+        IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.75f, editorArea); //$NON-NLS-1$
+        bottom.addView(RESULT_VIEW_ID);
+		bottom.addView(PLAN_VIEW_ID);
+	}
+}
