6724766e030392dd72424a5ac59fb1a6eed22d27
Brian Payton <bpayton@us.ibm.com>
2010-05-25 21:54:43 +0000

    [314318] Modified "criteria" classes in plugin o.e.d.sqltools.sqlbuilder to make classes and methods public to enhance reusability

--- plugins/org.eclipse.datatools.sqltools.sqlbuilder/src/org/eclipse/datatools/sqltools/sqlbuilder/views/criteria/CriteriaModifier.java
+++ plugins/org.eclipse.datatools.sqltools.sqlbuilder/src/org/eclipse/datatools/sqltools/sqlbuilder/views/criteria/CriteriaModifier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright © 2000, 2007 IBM Corporation and others.
+ * Copyright © 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which is available at
