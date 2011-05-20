05d116fe4f062edafdc4e6684eeea1b899e27b69
Linda Chan <lchan@actuate.com>
2011-05-20 00:47:24 +0000

    [346366] Expose visibility of an overriding oda.consumer.driverBridge extension in an ODA design session

---
+++ tests/org.eclipse.datatools.connectivity.oda.consumer.tests/src/org/eclipse/datatools/connectivity/oda/consumer/test/impl/DummyDriver.java
@@ -0,0 +1,72 @@
+/*
+ *************************************************************************
+ * Copyright (c) 2011 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation - initial API and implementation
+ *  
+ *************************************************************************
+ */
+
+package org.eclipse.datatools.connectivity.oda.consumer.test.impl;
+
+import java.util.Map;
+
+import org.eclipse.datatools.connectivity.oda.IConnection;
+import org.eclipse.datatools.connectivity.oda.IDriver;
+import org.eclipse.datatools.connectivity.oda.LogConfiguration;
+import org.eclipse.datatools.connectivity.oda.OdaException;
+
+/**
+ *  Dummy IDriver implementation as a placeholder in the oda.datasource extension.
+ */
+public class DummyDriver implements IDriver
+{
+    public static final String DRIVER_NAME_CONTEXT_KEY = "DummyDriver.className"; //$NON-NLS-1$
+    
+    /* (non-Javadoc)
+     * @see org.eclipse.datatools.connectivity.oda.IDriver#getConnection(java.lang.String)
+     */
+    public IConnection getConnection( String dataSourceId ) throws OdaException
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.datatools.connectivity.oda.IDriver#setLogConfiguration(org.eclipse.datatools.connectivity.oda.LogConfiguration)
+     */
+    public void setLogConfiguration( LogConfiguration logConfig )
+            throws OdaException
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.datatools.connectivity.oda.IDriver#getMaxConnections()
+     */
+    public int getMaxConnections() throws OdaException
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.datatools.connectivity.oda.IDriver#setAppContext(java.lang.Object)
+     */
+    public void setAppContext( Object context ) throws OdaException
+    {
+        if( context instanceof Map )
+        {
+            Map contextMap = (Map)context;
+            ((Map) context).put( DRIVER_NAME_CONTEXT_KEY, getClass().getName() );
+        }
+
+    }
+
+}
