75a7207fadf5170f01b4cb512cbe8828f33b0791
Pierre Queinnec <pierre.queinnec@zenika.com>
2012-02-22 14:56:35 +0100

    [368696] Add support for authorization identifiers.
    Signed-off-by: Pierre Queinnec <pierre.queinnec@zenika.com>

--- plugins/org.eclipse.datatools.enablement.mysql/src/org/eclipse/datatools/enablement/mysql/catalog/MySqlCatalogDatabase.java
+++ plugins/org.eclipse.datatools.enablement.mysql/src/org/eclipse/datatools/enablement/mysql/catalog/MySqlCatalogDatabase.java
@@ -9,10 +9,19 @@
  ******************************************************************************/
 package org.eclipse.datatools.enablement.mysql.catalog;
 
+import java.lang.ref.SoftReference;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.eclipse.datatools.connectivity.sqm.core.definition.DatabaseDefinition;
 import org.eclipse.datatools.connectivity.sqm.core.rte.RefreshManager;
 import org.eclipse.datatools.connectivity.sqm.core.rte.jdbc.JDBCDatabase;
+import org.eclipse.datatools.connectivity.sqm.core.util.CatalogLoaderOverrideManager;
+import org.eclipse.datatools.connectivity.sqm.internal.core.RDBCorePlugin;
+import org.eclipse.datatools.connectivity.sqm.loader.JDBCBaseLoader;
+import org.eclipse.datatools.enablement.mysql.catalog.loaders.MySqlAuthorizationIdentifierLoader;
+import org.eclipse.datatools.modelbase.sql.accesscontrol.SQLAccessControlPackage;
 import org.eclipse.datatools.modelbase.sql.schema.SQLSchemaPackage;
 import org.eclipse.datatools.modelbase.sql.schema.Schema;
 import org.eclipse.emf.common.util.EList;
@@ -26,6 +35,9 @@ public class MySqlCatalogDatabase extends JDBCDatabase {
 	
 	private Boolean schemasLoaded = Boolean.FALSE;
 	private Boolean catalogsLoaded = Boolean.FALSE;
+	private Boolean authorizationIdsLoaded = Boolean.FALSE;
+
+	private transient SoftReference authorizationIdLoaderRef;
 
 	public MySqlCatalogDatabase(Connection connection){
 		super(connection);
@@ -35,6 +47,12 @@ public class MySqlCatalogDatabase extends JDBCDatabase {
 		synchronized (schemasLoaded) {
 			if (schemasLoaded.booleanValue()) {
 				schemasLoaded = Boolean.FALSE;
+			}
+		}
+
+		synchronized (authorizationIdsLoaded) {
+			if (authorizationIdsLoaded.booleanValue()) {
+				authorizationIdsLoaded = Boolean.FALSE;
 			}
 		}
 
@@ -70,4 +88,71 @@ public class MySqlCatalogDatabase extends JDBCDatabase {
 		}
 		return catalogs;
 	}
+
+	public EList getAuthorizationIds() {
+		synchronized (this.authorizationIdsLoaded) {
+			if (!authorizationIdsLoaded.booleanValue())
+				this.loadAuthorizationIdentifiers();
+		}
+
+		return super.getAuthorizationIds();
+	}
+
+	private void loadAuthorizationIdentifiers() {
+		boolean deliver = eDeliver();
+		try {
+			List container = super.getAuthorizationIds();
+			List existingAuthorizationIds = new ArrayList(container);
+
+			eSetDeliver(false);
+
+			container.clear();
+			getAuthorizationIdentifierLoader().loadAuthorizationIdentifiers(container, existingAuthorizationIds);
+			getAuthorizationIdentifierLoader().clearAuthorizationIdentifiers(existingAuthorizationIds);
+
+			authorizationIdsLoaded = Boolean.TRUE;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			eSetDeliver(deliver);
+		}
+	}
+
+	protected final MySqlAuthorizationIdentifierLoader getAuthorizationIdentifierLoader() {
+		// cache the AuthorizationIdentifierLoader for better performance
+		if (authorizationIdLoaderRef == null || authorizationIdLoaderRef.get() == null) {
+			authorizationIdLoaderRef = new SoftReference(createAuthorizationIdentifierLoader());
+		}
+
+		return (MySqlAuthorizationIdentifierLoader) authorizationIdLoaderRef.get();
+	}
+
+	/**
+	 * Creates and returns an instance of the AuthorizationIdentifierLoader. By default an instance of the
+	 * <code>MySqlAuthorizationIdentifierLoader</code> is returned. This behavior can be changed by providing an
+	 * <code>overrideLoader</code> using the eclass org.eclipse.datatools.modelbase.sql.accesscontrol.
+	 * AuthorizationIdentifier.
+	 * 
+	 * @return An instance of MySqlAuthorizationIdentifierLoader.
+	 */
+	private MySqlAuthorizationIdentifierLoader createAuthorizationIdentifierLoader() {
+		// get the database definition for the actual database
+		DatabaseDefinition databaseDefinition = RDBCorePlugin.getDefault().getDatabaseDefinitionRegistry()
+				.getDefinition(this.getCatalogDatabase());
+
+		// see if someone is interested in providing an own authorization identifier loader
+		JDBCBaseLoader loader = CatalogLoaderOverrideManager.INSTANCE.getLoaderForDatabase(databaseDefinition,
+				SQLAccessControlPackage.eINSTANCE.getAuthorizationIdentifier().getInstanceClassName());
+
+		if (loader != null) {
+			MySqlAuthorizationIdentifierLoader authorizationIdLoader = (MySqlAuthorizationIdentifierLoader) loader;
+			authorizationIdLoader.setCatalogObject(this);
+			return authorizationIdLoader;
+		}
+
+		return new MySqlAuthorizationIdentifierLoader(this);
+	}
+
 }
---
+++ plugins/org.eclipse.datatools.enablement.mysql/src/org/eclipse/datatools/enablement/mysql/model/impl/MySqlUserImpl.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Zenika
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: queinnec - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.datatools.enablement.mysql.model.impl;
+
+import org.eclipse.datatools.enablement.mysql.model.MySqlUser;
+import org.eclipse.datatools.modelbase.sql.accesscontrol.impl.UserImpl;
+
+/**
+ * A MySQL user definition.
+ * 
+ * @author pierre.queinnec@zenika.com
+ */
+public class MySqlUserImpl extends UserImpl implements MySqlUser {
+
+}
