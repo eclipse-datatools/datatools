1e7189d97695c378ff0776589e84dd11dec6af48
Linda Chan <lchan@actuate.com>
2014-02-25 17:50:21 -0800

    Removed more targets for incubator packaging; fixed build script error

--- plugins/org.eclipse.datatools.releng.builder/extras/CheckDTP_PotentialVersionIssue.sh
+++ plugins/org.eclipse.datatools.releng.builder/extras/CheckDTP_PotentialVersionIssue.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#
 # Copyright (c) 2011, 2014 Actuate Corporation.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
