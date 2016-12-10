6fea15a2245336fcef1db1966b635a8ed47896ad
Brian Payton <bpayton@us.ibm.com>
2016-12-09 20:29:06 -0800

    509011: Update copyright statements

--- plugins/org.eclipse.datatools.enablement.ibm.db2/src/org/eclipse/datatools/enablement/ibm/db2/ddl/DB2DdlParser.java
+++ plugins/org.eclipse.datatools.enablement.ibm.db2/src/org/eclipse/datatools/enablement/ibm/db2/ddl/DB2DdlParser.java
@@ -1,12 +1,13 @@
-/*
- *+------------------------------------------------------------------------+
- *| Licensed Materials - Property of IBM                                   |
- *| (C) Copyright IBM Corp. 2003, 2012.  All Rights Reserved.              |
- *|                                                                        |
- *| US Government Users Restricted Rights - Use, duplication or disclosure |
- *| restricted by GSA ADP Schedule Contract with IBM Corp.                 |
- *+------------------------------------------------------------------------+
- */
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.datatools.enablement.ibm.db2.ddl;
 
 import java.util.Iterator;
