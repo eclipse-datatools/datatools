0728dc025b638f1d58af640cf29118f695f6bfd9
nickboldt <nboldt@redhat.com>
2018-05-04 09:48:11 -0400

    run tests via a suite so we can more easily disable the failing ones - tests/org.eclipse.datatools.connectivity.oda.consumer.tests
    Signed-off-by: nickboldt <nboldt@redhat.com>

---
+++ tests/org.eclipse.datatools.connectivity.oda.consumer.tests/src/org/eclipse/datatools/connectivity/oda/consumer/tests/AllTestSuites.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat Inc and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *******************************************************************************/
+
+package org.eclipse.datatools.connectivity.oda.consumer.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTestSuites extends TestSuite {
+	public static Test suite() {
+		return new AllTestSuites();
+	}
+
+	public AllTestSuites() {
+		super("Unit Tests");
+//	    addTestSuite(AdvQueryTest.class);
+//	    addTestSuite(AppContextTest.class);
+		addTestSuite(BinaryCompatibiltyTest.class);
+//	    addTestSuite(ConnectionTest.class);
+		addTestSuite(ConsumerMessagesTest.class);
+//	    addTestSuite(DataSetMetaDataTest.class);
+//	    addTestSuite(DriverBridgeTest.class);
+//	    addTestSuite(DriverTest.class);
+		addTestSuite(FlatFileTestCase.class);
+//	    addTestSuite(LoggerInstanceTest.class);
+		addTestSuite(OdaBlobTest.class);
+		addTestSuite(OdaClobTest.class);
+//	    addTestSuite(OutputParametersTest.class);
+//	    addTestSuite(ParameterRowSetTest.class);
+//	    addTestSuite(ParamMetaDataTest.class);
+//	    addTestSuite(PropertyProviderTest.class);
+//	    addTestSuite(QueryTest.class);
+//	    addTestSuite(ResultSetMetaDataTest.class);
+//	    addTestSuite(ResultSetTest.class);
+//	    addTestSuite(TraceLogConfigTest.class);
+		addTestSuite(TraceLogTest.class);
+	}
+
+}
