209258c8e9bf1b23d266aa518e47864250bb2bfe
nickboldt <nboldt@redhat.com>
2018-11-01 16:51:54 -0400

    Bug 540698 - update to EPL 2.0
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- plugins/connectivity/org.eclipse.datatools.connectivity.apache.derby.ui/src/org/eclipse/datatools/connectivity/apache/derby/internal/ui/connection/DerbyEmbeddedDBProfileDetailsWizardPage.java
+++ plugins/connectivity/org.eclipse.datatools.connectivity.apache.derby.ui/src/org/eclipse/datatools/connectivity/apache/derby/internal/ui/connection/DerbyEmbeddedDBProfileDetailsWizardPage.java
@@ -2,9 +2,9 @@
  * Copyright (c) 2004-2006, 2007 Sybase, Inc.
  * 
  * All rights reserved. This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License v1.0 which
+ * available under the terms of the Eclipse Public License 2.0 which
  * accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
  * 
  * Contributors: brianf - initial API and implementation
  *     IBM Corporation - migrated to new wizard framework
--- plugins/enablement/org.eclipse.datatools.enablement.sybase.ui/src/org/eclipse/datatools/enablement/sybase/ui/util/DSEUtil.java
+++ plugins/enablement/org.eclipse.datatools.enablement.sybase.ui/src/org/eclipse/datatools/enablement/sybase/ui/util/DSEUtil.java
@@ -2,8 +2,8 @@
  * Copyright (c) 2004, 2005,2008 Sybase, Inc. and others.
  * 
  * All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
- * Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Public License 2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
  * 
  * Contributors: Sybase, Inc. - initial API and implementation
  **********************************************************************************************************************/
